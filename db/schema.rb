37593550091c3c44c1960501f574f215f97f616c
BenRobOsborne <benosborne605@gmail.com>
2019-01-16 07:56:48 +0100

    4.2 renamed color colour in products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_132304) do
+ActiveRecord::Schema.define(version: 2019_01_16_064330) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2019_01_15_132304) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color"
+    t.string "colour"
     t.decimal "price"
   end
 
