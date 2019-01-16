7f65d588140e4812f2b386f6a1e36ef705040dbd
BenRobOsborne <benosborne605@gmail.com>
2019-01-16 16:00:04 +0100

    4.5 dynamic carousel partials cleaned up

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,9 +1,8 @@
 class SimplePagesController < ApplicationController
   def index
   end
-end
-class SimplePagesController < ApplicationController
+
   def landing_page
-    @featured_product = Product.first
+    @products = Product.limit(4)
   end
 end
