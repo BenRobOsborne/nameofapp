a0eaf7c9c85d1427daa1310bce33ab5b05779b76
BenRobOsborne <benosborne605@gmail.com>
2019-02-12 14:27:53 +0100

    stars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery3
-//= require jquery.raty.js
 //= require jquery_ujs
+//= require jquery.raty.js
 //= require activestorage
 //= require turbolinks
 //= require_tree ../../../vendor/assets/javascripts
