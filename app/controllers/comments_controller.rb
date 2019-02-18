a225550ae4d5397f784b0695b427f21a13937884
BenRobOsborne <benosborne605@gmail.com>
2019-02-18 10:51:12 +0100

    5.8 issue with pg resolved star fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
+
+
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
     @user = current_user
-    
+
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
