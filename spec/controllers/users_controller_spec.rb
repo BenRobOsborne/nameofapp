cdf8c0fc7e6049603cb604bc9bb5e90df96582e3
BenRobOsborne <benosborne605@gmail.com>
2019-02-19 12:24:33 +0100

    6.3 functionl test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
+
   let(:user) { User.create!(email: 'peter@example.com', password: '1234567890') }
   let(:user2) { User.create!(email: 'barb@gmail.com', password: 'secret') }
 
@@ -14,6 +15,12 @@ describe UsersController, type: :controller do
          get :show, params: { id: user.id }
          expect(response).to be_ok
          expect(assigns(:user)).to eq user
+       end
+
+       it "cannot access the other user's details" do
+         get :show, params: {id: user2.id}
+         expect(response).to have_http_status(302)
+         expect(response).to redirect_to root_path
        end
      end
 
