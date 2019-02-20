cd231aaa2e3fc9627d93240bc3ed9684c82f5680
BenRobOsborne <benosborne605@gmail.com>
2019-02-20 15:05:55 +0100

    5.3 revised mailgun install

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,5 +13,11 @@ class UserMailer < ApplicationMailer
     mail(:to => user.email, :subject=> "Welcome to #{@appname}!")
   end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 
 end
