ac51454c31e08ca909d59eae6f087169c63aa6ad
BenRobOsborne <benosborne605@gmail.com>
2019-02-25 08:18:08 +0100

    6.6 payment controller corrections

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,6 @@
 class PaymentsController < ApplicationController
   def create
-    @product = Product.find(params{:product_id})
+    @product = Product.find(params[:product_id])
     @user = current_user
   token = params[:stripeToken]
   # Create the charge on Stripe's servers - this will charge the user's card
@@ -9,7 +9,7 @@ class PaymentsController < ApplicationController
       amount: (@product.price*100).to_i, # amount in cents, again
       currency: "usd",
       source: token,
-      description: params[:stripeEmail]
+      description: params[:stripeEmail],
       receipt_email: @user.email
     )
 
